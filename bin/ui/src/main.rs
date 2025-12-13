64a7b27d201b3b2f447781a55e8d1a6d92d85194
Benjamin Everest <everest.bt@gmail.com>
2025-12-13 14:34:21 +0000

    Make a selectable list

--- bin/ui/src/main.rs
+++ bin/ui/src/main.rs
@@ -3,6 +3,18 @@ use api::game_fetch;
 use eframe::egui;
 use std::env;
 
+#[derive(Clone)]
+struct GameListItem {
+    game: game_fetch::Game,
+    selected: bool,
+}
+
+impl GameListItem {
+    fn select(&mut self) {
+        self.selected = !self.selected;
+    }
+}
+
 fn main() -> eframe::Result {
     let runtime = tokio::runtime::Runtime::new().expect("Unable to create a runtime");
 
@@ -15,24 +27,27 @@ fn main() -> eframe::Result {
         ..Default::default()
     };
 
-    let mut owned_games: Vec<game_fetch::Game> = runtime.block_on(game_fetch::get_owned_games(&key, &steam_id));
-    owned_games.sort_by(|a,b| a.name.cmp(&b.name));
-
-    eframe::run_simple_native("My egui App", options, move |ctx, _frame| {
+    // bool == selected
+    let mut game_list: Vec<GameListItem> = runtime.block_on(game_fetch::get_owned_games(&key, &steam_id)).iter().map(|g| GameListItem { game: g.clone(), selected: false }).collect();
+    game_list.sort_by(|a,b| a.game.name.cmp(&b.game.name));
+ 
+    eframe::run_simple_native("Steam randomiser", options, move |ctx, _frame| {
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.heading("Welcome to Steam Randomiser");
+            // Display the first selected game
+            let selected = game_list.iter().filter(|a| a.selected);
+            for s in selected {
+                ui.add(egui::Label::new(s.game.name.clone()));
+            }            
             egui::ScrollArea::vertical().show(ui, |ui| {
-                for game in &owned_games {
-                    // Add some spacing to let it breathe
+                for game in &mut game_list {
                     ui.add_space(5.0);
-
-                    // Add a clickable label using egui::Label::sense()
+                    // Add a clickable game using egui::Label::sense()
                     if ui
-                        .add(egui::Label::new(&game.name).sense(egui::Sense::click()))
+                        .add(egui::Label::new(&game.game.name).sense(egui::Sense::click()))
                         .clicked() {
+                            game.select();
                     };
-
-                    // Add some spacing to let it breathe
                     ui.add_space(5.0);
                 }
             });
