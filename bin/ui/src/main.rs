37843da7332c0d06ac4739f975861cc4384713fb
Benjamin Everest <everest.bt@gmail.com>
2025-12-13 17:29:49 +0000

    Display goals when game selected

--- bin/ui/src/main.rs
+++ bin/ui/src/main.rs
@@ -1,4 +1,5 @@
 use api::game_fetch;
+use db::achievement_store;
 
 use eframe::egui;
 use std::env;
@@ -27,30 +28,56 @@ fn main() -> eframe::Result {
         ..Default::default()
     };
 
-    // bool == selected
     let mut game_list: Vec<GameListItem> = runtime.block_on(game_fetch::get_owned_games(&key, &steam_id)).iter().map(|g| GameListItem { game: g.clone(), selected: false }).collect();
     game_list.sort_by(|a,b| a.game.name.cmp(&b.game.name));
+
+    let goals: Vec<achievement_store::Achievement> = achievement_store::get_achievements().expect("Failed to load achievements");
  
     eframe::run_simple_native("Steam randomiser", options, move |ctx, _frame| {
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.heading("Welcome to Steam Randomiser");
-            // Display the first selected game
-            let selected = game_list.iter().filter(|a| a.selected);
-            for s in selected {
-                ui.add(egui::Label::new(s.game.name.clone()));
-            }            
-            egui::ScrollArea::vertical().show(ui, |ui| {
-                for game in &mut game_list {
-                    ui.add_space(5.0);
-                    // Add a clickable game using egui::Label::sense()
-                    if ui
-                        .add(egui::Label::new(&game.game.name).sense(egui::Sense::click()))
-                        .clicked() {
-                            game.select();
-                    };
-                    ui.add_space(5.0);
-                }
-            });
+
+            ui.with_layout(egui::Layout::left_to_right(egui::Align::TOP), |ui| {
+                // Display the first selected game
+                let selected = game_list.iter().filter(|a| a.selected);
+                ui.with_layout(egui::Layout::top_down(egui::Align::TOP), |ui| {
+                    // List out goals for each selected item    
+                    for s in selected {
+                        ui.add(egui::Label::new(s.game.name.clone()));
+                        ui.add_space(5.0);
+                        // Goals
+                        ui.label("Goals:");
+                        ui.add_space(5.0);
+                        let game_goals = goals.iter().filter(|a| a.app_id == s.game.appid);
+                        for a in game_goals {
+                            let result;
+                            if a.description.is_some() {
+                                result = format!("{} : {}", a.display_name.clone(), a.description.clone().unwrap());
+                            }
+                            else {
+                                result = format!("{}", a.display_name.clone());
+                            }
+                            ui.label(result);
+                            ui.add_space(5.0);
+                        }
+                        ui.add_space(5.0);
+                    }   
+                });      
+                egui::ScrollArea::vertical().show(ui, |ui| {
+                    ui.with_layout(egui::Layout::top_down(egui::Align::TOP), |ui| {
+                        for game in &mut game_list {
+                            ui.add_space(5.0);
+                            // Add a clickable game using egui::Label::sense()
+                            if ui
+                                .add(egui::Label::new(&game.game.name).sense(egui::Sense::click()))
+                                .clicked() {
+                                    game.select();
+                            };
+                            ui.add_space(5.0);
+                        }
+                    });
+                });
+            });   
         });
     })
 }
