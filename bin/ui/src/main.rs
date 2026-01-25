886a2fd990bc485b2e1db0d00b1124f354855e42
Benjamin Everest <everest.bt@gmail.com>
2026-01-25 16:08:51 +0000

    Add filter for game with achievements

--- bin/ui/src/main.rs
+++ bin/ui/src/main.rs
@@ -36,6 +36,7 @@ fn main() -> eframe::Result {
 
     // Filters
     let mut filter_completed_game : bool = false;
+    let mut filter_has_achievements : bool = true;
 
     eframe::run_simple_native("Steam randomiser", options, move |ctx, _frame| {
         egui::CentralPanel::default().show(ctx, |ui| {
@@ -106,10 +107,16 @@ fn main() -> eframe::Result {
                 egui::ScrollArea::vertical().show(ui, |ui| {
                     ui.with_layout(egui::Layout::top_down(egui::Align::LEFT), |ui| {
                         ui.checkbox(&mut filter_completed_game, "Completed");
+                        ui.checkbox(&mut filter_has_achievements, "Has achievements");
                         for game in &mut game_list {
                             // check all filters
                             if filter_completed_game {
                                 if completed_games_cache.get(&game.appid).map(|c| c.complete).unwrap_or(0) != 100 {
+                                    continue;
+                                }
+                            }
+                            if filter_has_achievements {
+                                if !completed_games_cache.get(&game.appid).map(|c| c.has_achievements).unwrap_or(true) {
                                     continue;
                                 }
                             }
